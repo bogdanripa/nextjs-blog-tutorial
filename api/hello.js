6d4d20631f2d03364855e00ded25e52f4a39d760
bogdanripa <bogdanripa@gmail.com>
2024-06-11 11:11:30 +0300

    function call

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,12 @@
-export function GET(request) {
-  return new Response(`Hello from ${process.env.VERCEL_REGION}`);
+export async function GET(request) {
+  let cnt = parseInt(request.url.replace(/^.*cnt=/, ''))
+  let body = 'DONE';
+  const nowInMs = Date.now();
+  if (cnt>0) {
+    cnt--;
+    //let response = await fetch(`http://localhost:3000/api/hello?cnt=${cnt}`);
+    let response = await fetch(`https://nextjs-blog-tutorial-cns6.vercel.app/api/hello?cnt=${cnt}`);
+    body = await response.text();
+  }
+  return new Response(`${cnt} - ${Date.now() - nowInMs}\n${body}`);
 }
